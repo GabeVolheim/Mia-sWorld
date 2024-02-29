de5ab7dc9410a955c17ce830f850a87cc34af426
GabeVolheim <gabevolheim@gmail.com>
2024-02-29 12:34:21 -0500

    Created initial UI

--- heymia.c
+++ heymia.c
@@ -1,43 +1,74 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <gtk/gtk.h>
 #include "hangman.h"
 #include "2048.h"
 
-int main(void)
+static void print_hello (GtkWidget *widget, gpointer data)
 {
-    char miaChoice;
-    for(;;)
-    {
-        // Initial menu
-        printf("Hello Mia\n");
-        printf("What do you want to do today?\n");
-        printf("1. Hangman\n2. 2048\n3. Quit\nYour Choice: ");
-        scanf(" %c", &miaChoice);
+  g_print ("Hello World\n");
+}
 
-        switch (miaChoice)
-        {
+static void activate (GtkApplication *app, gpointer user_data)
+{
+  GtkWidget *window;
+  GtkWidget *button;
+
+  window = gtk_application_window_new (app);
+  gtk_window_set_title (GTK_WINDOW (window), "Mias World");
+  gtk_window_set_default_size (GTK_WINDOW (window), 400, 400);
+
+  button = gtk_button_new_with_label ("Hello World");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+  gtk_window_set_child (GTK_WINDOW (window), button);
+
+  gtk_window_present (GTK_WINDOW (window));
+}
+
+int main(int argc, char **argv)
+{
+    GtkApplication *app;
+    int status;
+    char miaChoice;
+    app = gtk_application_new ("com.area52.MiasWorld", 0);
+    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+    status = g_application_run (G_APPLICATION (app), argc, argv);
+    g_object_unref (app);
+    return status;
+
+    // for(;;)
+    // {
+
+    //     // Initial menu
+    //     printf("Hello Mia\n");
+    //     printf("What do you want to do today?\n");
+    //     printf("1. Hangman\n2. 2048\n3. Quit\nYour Choice: ");
+    //     scanf(" %c", &miaChoice);
+
+    //     switch (miaChoice)
+    //     {
             
-        case '1':
-            // Plays hangman
-            playHangman();
-            break;
-        case '2':
-            // Plays 2048
-            play2048();
-            break;
-        case '3':
-            // Mia quits
-            puts("Okay bye loser :(");
-            return 0;
-            break;
-        default:
-            // Option is not availiable
-            puts("\nThats not an option bruh\n");
-            break;
-        }
-    }
-    return 1;
+    //     case '1':
+    //         // Plays hangman
+    //         playHangman();
+    //         break;
+    //     case '2':
+    //         // Plays 2048
+    //         play2048();
+    //         break;
+    //     case '3':
+    //         // Mia quits
+    //         puts("Okay bye loser :(");
+    //         return 0;
+    //         break;
+    //     default:
+    //         // Option is not availiable
+    //         puts("\nThats not an option bruh\n");
+    //         break;
+    //     }
+    // }
+    // return 1;
 }
 
 
--- makefile
+++ makefile
@@ -1,5 +1,6 @@
 COMPILER = gcc
-OPTIONS = -Wall -Werror
+OPTIONS = -Wall
+UI = `pkg-config --cflags --libs gtk4` -lgtk-4 -L/lib/x86_64-linux-gnu/gtk-4.0
 PROGRAM = heymia
 FILES = heymia.c 2048.c hangman.c
 
@@ -10,7 +11,7 @@ run: $(PROGRAM)
 	./$(PROGRAM)
 
 $(PROGRAM): $(FILES)
-	$(COMPILER) $(OPTIONS) $(FILES) -o $(PROGRAM)
+	$(COMPILER) $(OPTIONS) $(FILES) -o $(PROGRAM) $(UI)
 
 clean:
 	rm -f $(PROGRAM)
